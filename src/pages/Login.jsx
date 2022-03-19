df4727a0378cc8ef3640f684525c75a382fca8a4
axel alejandro morel <alejandro.morel1905@gmail.com>
2022-03-18 21:31:00 -0300

    React Shop

---
+++ src/pages/Login.jsx
@@ -0,0 +1,35 @@
+import React, {useRef } from 'react';
+import '../styles/Login.scss';
+import logo from '@logos/logo_yard_sale.svg';
+
+const Login = () =>{
+    const form = useRef(null);
+    const handleSubmit = (event) =>{
+        event.preventDefault();
+        const formDate = new FormData(form.current);
+        const data = {
+            username: formDate.get('email'),
+            password: formDate.get('password')
+        }
+        console.log(data);
+    }
+    return (
+        <div className="Login">
+            <div className="Login-container">
+            <img src={logo} alt="logo" className="logo"/>
+            <form action="/" className="form" ref={form}>
+                <label htmlFor="email" className='label'>Email address</label>
+                <input type="text" name='email' placeholder='alejandro@example.com' className='input input-email' />
+                <label htmlFor='password' className="label">Password</label>
+                <input type="password" name="password" placeholder="*********" className="input input-password"/>
+                <button className="primary-button login-button" onClick={handleSubmit}>
+                    Log in
+                </button>
+                <a href="/">Forgot my password</a>
+            </form>
+            <button className='secondary-button signup-button' >Sign up</button>
+            </div>
+        </div>
+    );
+}
+export default Login;
